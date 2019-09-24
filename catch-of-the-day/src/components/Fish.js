59af66d8b75e873fbb6032043d995217c2e42541
alexhokl <alexhokl@gmail.com>
2019-09-24 10:28:28 +0800

    Added PropTypes validations

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { formatPrice } from '../helpers';
 
 class Fish extends Component {
+  static propTypes = {
+    addToOrder: PropTypes.func,
+    index: PropTypes.string,
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number,
+    })
+  }
   add = () => {
     this.props.addToOrder(this.props.index);
   };
